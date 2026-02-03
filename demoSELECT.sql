2a76aa9c0acc4b6509b6cb0d8caeb86799a82b74
antlerjely96 <antlerjely96@gmail.com>
2026-02-03 15:39:47 +0700

    demo SELECT statements

--- demoSELECT.sql
+++ demoSELECT.sql
@@ -17,3 +17,18 @@ SELECT * FROM students WHERE address IN ('HN', 'HP', 'TPHCM');
 SELECT * FROM students WHERE address = 'HN' OR address = 'HP' OR address = 'TPHCM';
 /* Lấy thông tin các sinh viên có địa chỉ không nằm trong (HN, HP) */
 SELECT * FROM students WHERE address NOT IN ('HN', 'HP', 'TPHCM');
+/* Tìm kiếm, lấy các bản ghi có full_name bắt đầu là std */
+SELECT * FROM students WHERE full_name LIKE 'std%';
+/* Tìm kiếm, lấy các bản ghi có full_name kết thúc là 1 */
+SELECT * FROM students WHERE full_name LIKE '%1';
+/* Tìm kiếm, lấy các bản ghi có full_name chứa de */
+SELECT * FROM students WHERE full_name LIKE '%de%';
+/* Lấy các bản ghi trong bảng, sắp xếp theo thứ tự tên giảm dần */
+SELECT * FROM students ORDER BY full_name DESC;
+/* Lấy 3 bản ghi, bắt đầu từ vị trí 1 */
+SELECT * FROM students LIMIT 3 OFFSET 1;
+SELECT * FROM students LIMIT 1, 3;
+/* Lấy các bản ghi có phone_number không bị null */
+SELECT * FROM students WHERE phone_number IS NOT NULL;
+/* Lấy các bản ghi có phone_number bị null */
+SELECT * FROM students WHERE phone_number IS NULL;
